7c55f990ed9ac3e25148cbc85b3335ab7d8f5237
Beau Simensen <beau@dflydev.com>
2011-01-14 22:12:50 -0800

    Not sure if this makes sense or not, but this makes some code work. I need to write some unit tests for this. refs #58

--- lib/substrate_stones_OrderedUtil.php
+++ lib/substrate_stones_OrderedUtil.php
@@ -12,18 +12,18 @@ class substrate_stones_OrderedUtil {
     static protected function compare($o1, $o2) {
         $p1 = $o1 instanceof substrate_stones_IPriorityOrderedStone;
         $p2 = $o2 instanceof substrate_stones_IPriorityOrderedStone;
-        if ( $p1 && ! $p2 ) return -1;
-        if ( $p2 && ! $p1 ) return 1;
+        if ( $p1 && ! $p2 ) return 1;
+        if ( $p2 && ! $p1 ) return -1;
         $i1 = self::GET_STONE_ORDER($o1);
         $i2 = self::GET_STONE_ORDER($o2);
-        return ($i1 < $i2) ? -1 : ($i1 > $i2) ? 1 : 0;
+        return ($i1 < $i2) ? 1 : ($i1 > $i2) ? -1 : 0;
     }
     static public function SORT(array $toBeSorted) {
         usort($toBeSorted, array('substrate_stones_OrderedUtil', 'compare'));
         return $toBeSorted;
     }
     static protected function GET_STONE_ORDER($object) {
-        $order = $object instanceof substrate_stones_IOrderedStone ? object.getStoneOrder() : null;
-        return $order === null ? $order : self::$LOWEST_PRECEDENCE;
+        $order = $object instanceof substrate_stones_IOrderedStone ? $object->getStoneOrder() : null;
+        return $order !== null ? $order : self::$LOWEST_PRECEDENCE;
     }
 }
